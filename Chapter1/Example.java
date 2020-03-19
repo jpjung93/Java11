80f154eeaa0d39cba61ce58436eeddc17f5bb068
jpjung93 <jpjung93@gmail.com>
2020-03-18 21:00:57 -0500

    Printing simple statement

---
+++ Chapter1/Example.java
@@ -0,0 +1,10 @@
+/*
+    Simple java file that I'm running from command line
+*/
+
+public class Example {
+  public static void main(String[] args) {
+    // A Java program begins with a call to main(String[] args)
+    System.out.println("Java drives the web");
+  }
+}
